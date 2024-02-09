26c59d8b0e981d6c87ca7f52390b9ced76b6c672
Vasco <a12402@esviriato.pt>
2024-02-09 09:44:49 +0000

    ygdw

---
+++ site/assets/js/main.js
@@ -0,0 +1,9 @@
+const vasco = document.getElementsByClassName("vascotest")[0];
+
+function onMouseHover() {
+    vasco.style.display = "flex"
+}
+
+function onMouseLeave() {
+    vasco.style.display = "none"
+}
